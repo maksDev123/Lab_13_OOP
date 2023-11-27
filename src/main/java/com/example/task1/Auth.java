63c39bbf9354f492c4595798dd95a49c0e5b71e7
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2023-11-27 06:51:55 +0200

    Init repo

---
+++ src/main/java/com/example/task1/Auth.java
@@ -0,0 +1,5 @@
+package com.example.task1;
+
+public class Auth {
+    public
+}
---
+++ src/main/java/com/example/task1/TwitterUse.java
@@ -0,0 +1,14 @@
+package com.example.task1;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class TwitterUse {
+    private String UserEmail;
+    private String country;
+    private String lastActivateTime;
+
+}
